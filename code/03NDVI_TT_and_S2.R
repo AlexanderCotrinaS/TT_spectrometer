13f32655e3f9cc509021e1d7d0c7b6cf73954a3e
Alexander Cotrina-Sanchez <alexander.cotrina.sanchez@gmail.com>
2023-11-29 13:30:56 +0000

    Update plotting code

--- code/03NDVI_TT_and_S2.R
+++ code/03NDVI_TT_and_S2.R
@@ -6,7 +6,8 @@ sentinel2 <- read_csv("data/S2_san1_22.csv")
 s2_2022 <- sentinel2%>%filter(between(date, as.Date('2022-01-01'), as.Date('2022-12-01'))) # Filter by date
 
 s2_2022_NDVI <- s2_2022 %>% mutate(NDVI.s2 = (s2_2022$B8-s2_2022$B4)/
-                                                 (s2_2022$B8 + s2_2022$B4))  # NDVI calculation
+                                                 (s2_2022$B8 + s2_2022$B4))  # NDVI calculation NIR = B8 ; Red = B4
+
 #TT data ----------------------------------------------------------------
 TT_data_sa1 <- read_csv("data/TT_sa1_2022_median.csv")
 
@@ -20,8 +21,20 @@ ndvi_f <- function(data) {
 TT_NDVI_sa1 <- ndvi_f(TT_data_sa1)
 
 #### Combining TT+ and S2 data
+total_sa1_ndvi22 <- merge(s2_2022_NDVI,TT_NDVI_sa1, by="date")
 
-total_braca_ndvi21<- merge(braca21.ndvi,bracasentinel2_NDVI, by="date")
-total_braca_ndvi21 <- total_braca_ndvi21 %>% mutate(Plot = "Campo Braca", Year = "2021")
-write.csv(braca21.ndvi, "braca21.ndvi.csv")
+##Delete duplicated rows
+total_sa1_ndvi22 <- total_sa1_ndvi22 %>% filter(!duplicated(date))
+
+#write.csv(total_sa1_ndvi22, "sa1_22_ndvi_fusion.csv")
+
+#########
+ggplot(total_sa1_ndvi22, aes(x = date)) +
+  geom_point(aes(y = NDVI.s2, color = "NDVI_S2"), size = 2) +
+  geom_point(aes(y = NDVI, color = "NDVI_TT"), size = 2) +
+  labs(title = "NDVI TT+ and S2",
+       x = "Date",
+       y = "NDVI") +
+  scale_color_manual(values = c("NDVI_S2" = "#B8860B", "NDVI_TT" = "#32CD32"))+
+  theme_minimal()
 
