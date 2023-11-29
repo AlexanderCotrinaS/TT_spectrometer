2010ea872a4b51ffc5e3b21aff079b135b63c165
Alexander Cotrina-Sanchez <alexander.cotrina.sanchez@gmail.com>
2023-11-29 15:09:21 +0000

    Data Plot updated

--- code/03NDVI_TT_and_S2.R
+++ code/03NDVI_TT_and_S2.R
@@ -7,6 +7,7 @@ s2_2022 <- sentinel2%>%filter(between(date, as.Date('2022-01-01'), as.Date('2022
 
 s2_2022_NDVI <- s2_2022 %>% mutate(NDVI.s2 = (s2_2022$B8-s2_2022$B4)/
                                                  (s2_2022$B8 + s2_2022$B4))  # NDVI calculation NIR = B8 ; Red = B4
+                                                                             #s2_2022_NDVI <- s2_2022_NDVI %>% na.omit()
 
 #TT data ----------------------------------------------------------------
 TT_data_sa1 <- read_csv("data/TT_sa1_2022_median.csv")
@@ -19,6 +20,8 @@ ndvi_f <- function(data) {
 }
 
 TT_NDVI_sa1 <- ndvi_f(TT_data_sa1)
+
+#TT_NDVI_sa1 <- TT_NDVI_sa1[TT_NDVI_sa1$NDVI>= 0, ] %>% na.omit()
 
 #### Combining TT+ and S2 data
 total_sa1_ndvi22 <- merge(s2_2022_NDVI,TT_NDVI_sa1, by="date")
